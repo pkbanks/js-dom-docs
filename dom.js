08b8f7880d836b1c60d1b69f502b84291c5afa17
pkbanks <pkbanks@gmail.com>
2017-10-09 11:27:10 -0400

    working thru js-dom slides, with notes

--- dom.js
+++ dom.js
@@ -1,3 +1,56 @@
 
-console.log(document.getElementById('our-text-input'))
+// console.log(document.getElementById('our-text-input'))
+
+
+// window.addEventListener('load', function(){
+// 	console.log(document.getElementById('our-text-input'));
+// })
+
+
+// window.addEventListener('load', function(){
+// 	var textField = document.getElementById('our-text-input');
+// 	console.log(textField);
+// })
+
+
+// window.addEventListener('load', function(){
+// 	var textField = document.getElementById('our-text-input');
+// 	textField.addEventListener('change', function(){
+// 		console.log("something changed");
+// 		console.dir(arguments);
+// 	})
+// })
+
+
+// function onChangeHandler(arg){
+// 	console.log("something changed");
+// 	console.dir(arg);
+// }
+
+
+// function onChangeHandler(input){
+// 	console.log("something changed");
+// 	console.log(input.value);
+// }
+
+
+// window.addEventListener('load', function(){
+// 	var textField = document.getElementById('our-text-input');
+// 	textField.addEventListener('change', function(event){
+// 		console.log("something changed");
+// 		console.dir(event.target.value);
+// 	})
+// })
+
+
+// var textField = document.getElementById('our-text-input');
+// textField.value = "default value";
+
+
+window.addEventListener('load', function(){
+	var textField = document.getElementById('our-text-input');
+	textField.value = "default value";
+})
+
+
 
